ac9a786654670d27145add6facef33abef7f00ef
GilsonCoelho <coelho.gilson@gmail.com>
2023-04-08 13:05:56 -0300

    EnableFeignClients

--- src/main/java/br/com/gsc/addressAPI/AddressApiApplication.java
+++ src/main/java/br/com/gsc/addressAPI/AddressApiApplication.java
@@ -2,7 +2,9 @@ package br.com.gsc.addressAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class AddressApiApplication {
 
